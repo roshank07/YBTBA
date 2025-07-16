db85fb9491d36081f57e87c5e55f2d2fed25c62a
roshank07 <roshann.purbey@gmail.com>
2025-07-16 20:51:54 +0530

    first commit

---
+++ src/main/java/com/ybtba/demo/YbtbaApplication.java
@@ -0,0 +1,13 @@
+package com.ybtba.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YbtbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YbtbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ybtba/demo/YbtbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybtba.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YbtbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
